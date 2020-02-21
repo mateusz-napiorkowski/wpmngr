83bb669799ff6bd39acf3dd8a98ac7aa6f12e7ba
Mateusz Napiórkowski <mateusz.napiorkowski0@gmail.com>
2020-02-21 21:48:55 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wpmngr", # Replace with your own username
+    version="0.0.1",
+    author="Mateusz Napiorkowski",
+    author_email="mateusz.napiorkowski0@gmail.com",
+    description="Wallpaper manager for Xfce",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+    ],
+    python_requires='>=3.6',
+)
