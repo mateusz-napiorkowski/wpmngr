a64cf5ac2d666a090084e8fc8f8ed451bc1ca511
Mateusz Napiórkowski <mateusz.napiorkowski0@gmail.com>
2020-02-23 13:06:09 +0100

    Add setwp(set wallpaper) command

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="wpmngr", # Replace with your own username
+    name="wpmngr",
     version="0.0.1",
     author="Mateusz Napiorkowski",
     author_email="mateusz.napiorkowski0@gmail.com",
@@ -19,4 +19,12 @@ setuptools.setup(
         "Operating System :: POSIX :: Linux",
     ],
     python_requires='>=3.6',
+    include_package_data=True,
+    install_requires=[
+        'Click',
+    ],
+    entry_points='''
+            [console_scripts]
+            setwp=wpmngr.wpmngr:setwp
+        ''',
 )
---
+++ wpmngr/wpmngr.py
@@ -0,0 +1,12 @@
+import click
+import subprocess
+
+@click.command()
+@click.argument('wallpaper_path', type=click.Path(exists=True))
+def setwp(wallpaper_path):
+    """Set a Wallpaper"""
+    paths = subprocess.getoutput('xfconf-query --channel xfce4-desktop --list |grep -E \"image-path|last-image\"'
+                                 ).split('\n')
+    for path in paths:
+        command = ['xfconf-query', '--channel', 'xfce4-desktop', '--property', path, '--set', wallpaper_path]
+        subprocess.run(command)
